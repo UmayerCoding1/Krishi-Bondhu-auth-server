683e886410ff253457fd493d70ad5c5d36519315
umayerCoding1 <umayerpro175@gmail.com>
2026-05-02 11:39:48 +0600

    feat: initialize express server with database connection and environment configuration

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
+
 async function main() {
   try {
-    await connectDB()
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
